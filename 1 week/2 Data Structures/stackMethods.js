4dd196b80dfa5fcf1ef5a4227242dfe772503957
Daria <dasha.nebogina@mail.ru>
2025-07-28 23:15:24 +0300

    feat: implement checkbrackets function

--- 1 week/2 Data Structures/stackMethods.js
+++ 1 week/2 Data Structures/stackMethods.js
@@ -30,14 +30,43 @@ class Stack {
     }
 }
 
-const stack = new Stack();
-stack.push(1);
-stack.push(2);
-stack.push(3);
-stack.push(5);
-stack.pop();
-stack.pop();
-console.log(stack.size());
-console.log(stack.isEmpty());
-console.log(stack.peek());
-console.log(stack);
+function checkBrackets(str) {
+    if (str.length % 2 !== 0) {
+        return false;
+    }
+    const stack = new Stack();
+    const bracketsArray = str.split('');
+    const brackets = {
+        '(': ')',
+        '[': ']',
+        '{': '}'
+    };
+    for (let i = 0; i <= bracketsArray.length - 1; i += 1) {
+        if (brackets[bracketsArray[i]]) {
+            stack.push(bracketsArray[i]);
+        }
+        else if (Object.values(brackets).includes(bracketsArray[i])) {
+            if (stack.isEmpty()) {
+                return false;
+            }
+            if (brackets[stack.pop()] !== bracketsArray[i]) {
+                return false;
+            }
+        }
+    }
+    return stack.isEmpty();
+}
+
+console.log(checkBrackets("({[]}))"));
+
+// const stack = new Stack();
+// stack.push(1);
+// stack.push(2);
+// stack.push(3);
+// stack.push(5);
+// stack.pop();
+// stack.pop();
+// console.log(stack.size());
+// console.log(stack.isEmpty());
+// console.log(stack.peek());
+// console.log(stack);
