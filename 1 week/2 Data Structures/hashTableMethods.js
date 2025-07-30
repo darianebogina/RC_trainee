ed337a9ecec5e55d9c1d766d4d98151ab466c2af
Daria <dasha.nebogina@mail.ru>
2025-07-30 16:40:05 +0300

    feat: implement hashTable

---
+++ 1 week/2 Data Structures/hashTableMethods.js
@@ -0,0 +1,82 @@
+class HashTable {
+    constructor() {
+        this.array = new Array(150);
+    }
+
+    _getBucket(key) {
+        const index = hash(key) % this.array.length;
+        return { index, bucket: this.array[index] };
+    }
+
+    set(key, value) {
+        const obj = this._getBucket(key);
+        const index = obj.index;
+        const bucket = obj.bucket || [];
+        if (!bucket) {
+            this.array[index] = bucket;
+        }
+        const found = bucket.find((element) => element[0] === key); //[key, value] | undefined
+        if (!found) {
+            bucket.push([key, value]);
+        } else {
+            found[1] = value;
+        }
+    }
+
+    get(key) {
+        const bucket = this._getBucket(key).bucket;
+        if (!bucket) {
+            return undefined;
+        }
+        const found = bucket.find((element) => element[0] === key); 
+        if (!found) {
+            return undefined;
+        } else {
+            return found[1];
+        }
+    }
+
+    delete(key) {
+        const bucket = this._getBucket(key).bucket;
+        if (!bucket) {
+            return false;
+        }
+        const foundIndex = bucket.findIndex((element) => element[0] === key);
+         if (foundIndex === -1) {
+            return false;
+        } else {
+            bucket.splice(foundIndex, 1);
+            return true;
+        }
+    }
+
+    has(key) {
+        const bucket = this._getBucket(key).bucket;
+        if (!bucket) {
+            return false;
+        }
+        const found = bucket.find((element) => element[0] === key); 
+        return !!found;
+    }
+}
+
+function hash(key) {
+    return key
+        .toString()
+        .split('')
+        .map(char => char.charCodeAt(0))
+        .reduce((acc, code) => (17 + acc + code * 2), 1);
+}
+
+const hashTable = new HashTable();
+hashTable.set(3, "value");
+hashTable.set("apple", "sweet");
+hashTable.set(3, "haha");
+console.dir(hashTable, { depth: null });
+console.log(hashTable.get(3));
+console.log(hashTable.get(1));
+console.log(hashTable.delete("apple"));
+console.log(hashTable.delete(1));
+console.dir(hashTable, { depth: null });
+console.log(hashTable.has(3));
+console.log(hashTable.has("prikol"));
